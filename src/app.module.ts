b6403ee8e72d3bd6b9c20dae7ec95c3194d7fef8
Shodi <hikihenrique@yahoo.com>
2019-12-02 23:47:39 -0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
 import { Config_Token } from './server/config';
+import { MessageController } from './message/message.controller';
+import { MessageService } from './message/message.service';
 
 const { DATABASE_USERNAME, DATABASE_PASSWORD } = process.env;
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(<SqliteConnectionOptions>{
-      type: 'sqlite',
-      database: './src/database/db',
-      entities: [],
-      logging: true
-    })
-  ],
-  providers: [Config_Token]
+  imports: [],
+  controllers: [MessageController],
+  providers: [MessageService, Config_Token]
 })
 export class AppModule {}
